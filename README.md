44eda7dd9cd6841b7e77148663d40e5337bd223c
Diego Albo Martínez <diego.albo.martinez@gmail.com>
2020-03-24 10:37:40 +0100

    Affine transformations (#6)
    closes issue #3 
    
    * Update create data with data creation as per paper
    
    - new file is for creation 30, 90 and 150k training data set
    - Update readme with new changes
    
    * added siamese notebook ang changed gitignore
    
    * changed directory structure
    
    * missed adding delete in commit
    
    * renamed some things
    
    * modified siamese-local for local env
    
    * testing gitbot
    
    * add requirements.txt
    
    * added tensorboard functionality
    
    * Reformulated the training set extraction
    
    * Reformulated the training set extraction
    
    * fix issue with random same and different letters
    
    * Finished affine transformations generation
    
    Co-authored-by: Nikhil Saldanha <nikhil.saldanha691@gmail.com>
    Co-authored-by: sharwinbobde <bsharwin@gmail.com>
    Co-authored-by: DiegoStock12 <diego.albo.martinez@gmail.com>

--- README.md
+++ README.md
@@ -8,8 +8,16 @@ Koch et. al. Keras implementation for reference: https://github.com/sorenbouma/k
 ## Setting up repository for development
 1. download `images_background` and `images_evaluation` from (here)[https://github.com/brendenlake/omniglot] and unzip into `data/raw`.
 2. create a virtual environment and install from `requirements.txt`
-3. run `create_data.py` to create data for training and evaluation.
+3. run `python create_data.py [30|60|90]` to create data for training. e.g., `python create_data.py 30` creates training data of size 30,000 in accordance with the paper and stores it in `data/processed/trainX_30k.npy` and `data/processed/trainY_30k.npy`. Code snippet to read the `.npy` file is included in the last part of `create_data.py`.
 
 ## Data Shape
 
-`create_data.py` converts the image from background and evaluation into numpy arrays of shape (number of letters, number of drawings of letters(20), height, width) and pickles them
+`create_data.py` converts the image from background and evaluation into numpy arrays of shape:
+
+trainX_30k: (30000, 2, 105, 105), trainY_30k: (30000, 1)
+
+trainX_90k: (90000, 2, 105, 105), trainY_90k: (90000, 1)
+
+trainX_150k: (150000, 2, 105, 105), trainY_150k: (150000, 1)
+
+Where index 0 is number of samples, index 1 is the pairs of images (1 pair, 2 images), index 2 and 3 are the height and width of the image
---
+++ create_train_data.py
@@ -0,0 +1,271 @@
+import glob
+import os
+import sys
+import imageio
+import numpy as np
+import pickle
+
+
+def extract_data(path):
+    """
+    Extracts the omniglot data from the path provided,
+    organizes it by alphabet and returns it
+    Parameters:
+    -----------
+    path: str
+        Path at which the dataset is available
+
+    Returns:
+    --------
+    alphabet_list: numpy.array shape(num_alphabets, num_letters, height, width)
+        The images of the letters from the path, organized by alphabet
+    """
+
+    # List to store images,
+    # one index for each alphabet
+    alphabet_list = []
+
+    # Iterate through alphabet folders
+    alphabet_paths = glob.glob(train_path + "/*")
+
+    # List of alphabets
+    alphs = [a for a in alphabet_paths]
+    # Choose 20 of the 40 alphabets to train on
+    train_alphabets = np.random.choice(alphs, size=20, replace=False)
+    # We should take these into account so we do not include them in validation or testing
+    # Keep just the other 20 alphabets
+    alphs = [a for a in alphs if a not in train_alphabets]
+    print("Length of remaining alphabets ", len(alphs))
+
+    # Get 10 for validation and the remaining for testing
+    validation_alphabets = np.random.choice(alphs, size=10, replace=False)
+    test_alphabets = [a for a in alphs if a not in validation_alphabets]
+
+    print(f"Alphabets to train on ({len(train_alphabets)}): ", train_alphabets)
+    print(
+        f"Alphabets to validate on ({len(validation_alphabets)}): ",
+        validation_alphabets,
+    )
+    print(f"Alphabets to test on ({len(test_alphabets)}): ", test_alphabets)
+
+    alphabet_groups = [train_alphabets, validation_alphabets, test_alphabets]
+    # array where the final results will be kept (train, val, test)
+    final_results = []
+
+    # Get the appropriate images for each alphabet
+    for i, group in enumerate(alphabet_groups):
+        print(f"{i}: {len(group)} --> {group}")
+        final_results.append([])
+        alphabet_list = []
+        for alphabet_path in group:
+            # Get name of alphabet
+            alphabet_name = alphabet_path.split("/")[-1]
+            # Create new entry in list of alphabets
+
+            alphabet_list.append([])
+
+            # Get list of letter folders under alphabet folder
+            letter_paths = glob.glob(alphabet_path + "/*")
+            print(f"reading {alphabet_name}...")
+
+            # Iterate through letter folders
+            for letter_path in letter_paths:
+                # Initialize list of images for all letters
+                # belonging to an alphabet
+                letter_images = []
+                # get list of images inside the current letter folder
+                image_paths = glob.glob(letter_path + "/*")
+
+                # iterate through images for the first 12 drawers (training)
+                # Get the appropriate slice for each group
+                if i == 0:
+                    # Training -> 12 drawers
+                    slice = image_paths[:12]
+                elif i == 1:
+                    # Validation -> 4 drawers
+                    slice = image_paths[12:16]
+                else:
+                    # Testing -> 4 drawers
+                    slice = image_paths[16:]
+                for image_path in slice:
+                    # read image and add to the list of images
+                    image = imageio.imread(image_path)
+                    letter_images.append(image)
+
+                # Append all images of letter to
+                # current index of alphabet list
+                alphabet_list[-1].append(np.stack(letter_images))
+
+            # create numpy array for images of all alphabets
+            # shape of np array each index: (20*num_letters, 105, 105)
+            alphabet_list[-1] = np.stack(alphabet_list[-1])
+
+        final_results[-1].append(alphabet_list)
+
+    return final_results
+
+
+if __name__ == "__main__":
+    """
+    Run with command `python create_train_data.py [30|60|90]`
+    """
+    print("Fixing the random seed to 42")
+    np.random.seed(42)
+
+    train_path = "data/raw/images_background"
+
+    alphabet_list = None
+    # train alphabet exists, load from file
+    if os.path.exists("data/processed/train_alphabet.pkl"):
+        with open("data/processed/train_alphabet.pkl", "rb") as f:
+            alphabet_list = pickle.load(f)
+        print(alphabet_list[0].shape)
+    # else, create it
+    else:
+        print("creating data for train...")
+        # alphabet_list = extract_data(train_path)
+        tr, val, test = extract_data(train_path)
+
+        # Save alphabet_list to pickle,
+        # since it takes a while to compute
+        with open("data/processed/train_alphabet.pkl", "wb") as f:
+            pickle.dump(tr[0], f)
+        with open("data/processed/validation_alphabet.pkl", "wb") as f:
+            pickle.dump(val[0], f)
+        with open("data/processed/test_alphabet.pkl", "wb") as f:
+            pickle.dump(test[0], f)
+
+    # read for argument from command line to determine
+    # the size of dataset required to be generated
+    size = None
+    if len(sys.argv) < 2:
+        print("must provide an argument for size of dataset (30, 90, or 150)")
+        sys.exit()
+    if sys.argv[1] in ("30", "90", "150"):
+        size = float(sys.argv[1])
+        print(f"size = {size}")
+    else:
+        print("argument must be one of 30, 90, or 150")
+        sys.exit()
+
+    # Check to delete the file before starting
+    if os.path.exists(f"data/processed/trainX_{int(size)}k.npy"):
+        print("Removing old files")
+        os.remove(f"data/processed/trainX_{int(size)}k.npy")
+        os.remove(f"data/processed/trainY_{int(size)}k.npy")
+
+    num_alphabets = len(alphabet_list)
+    alphabet_indexes = [i for i in range(num_alphabets)]
+    # compute the number of samples per alphabet
+    samples_per_alphabet = int((size * 1000) / num_alphabets)
+    print(samples_per_alphabet)
+
+    # Where we'll keep all the data
+    dataset = None
+    labels = None
+    # iterate through the images, alphabet wise
+    for i, alphabet in enumerate(alphabet_list):
+        # sample 1000 images at a time to prevent OOM
+        n_samples = 1500
+        final_res = None
+        alph_lab = None
+        for _ in range(int(samples_per_alphabet / n_samples)):
+            # gc.collect()
+
+            num_letters, num_drawings, _, _ = alphabet.shape
+
+            x1_alphabet = i
+            # randomly draw `n_samples` samples
+            x1_letters = np.random.choice(num_letters, size=n_samples)
+            x1_drawers = np.random.choice(num_drawings, size=n_samples)
+            x1_images = []
+            # fetch samples and append to `alphabets1`
+            for l, d in zip(x1_letters, x1_drawers):
+                x1_images.append(alphabet[l, d])
+
+            x2_alphabets = []
+            x2_letters = []
+            x2_drawers = []
+            x2_images = []
+
+            # make sure half samples have same letter
+            # i.e. same alphabet, may or may not have same drawing.
+            # for other half, make sure sample has different letter.
+            # we need to be careful cause not all alphabets have the same number of letters
+
+            # first half, same letter
+            for index in range(n_samples // 2):
+                # same alphabet
+                x2_alphabets.append(i)
+                # same
+                x2_letters.append(x1_letters[index])
+                x2_drawers.append(np.random.choice(range(num_drawings)))
+                x2_images.append(alphabet_list[i][x2_letters[-1]][x2_drawers[-1]])
+            # second half, different letter
+            for index in range(n_samples // 2, n_samples):
+                # choose a random alphabet
+                x2_alphabet_index = np.random.choice(range(num_alphabets))
+                x2_alphabets.append(x2_alphabet_index)
+
+                # get number of letters in alphabet
+                n_letters = alphabet_list[x2_alphabet_index].shape[0]
+
+                # if x2 alphabet is same as x1
+                if x2_alphabet_index == i:
+                    # make sure that letter is different
+                    x2_letter_index = (
+                        x1_letters[index] + np.random.randint(1, n_letters)
+                    ) % n_letters
+                    x2_letters.append(x2_letter_index)
+                # if x2 alphabet is different to x1
+                else:
+                    # pick a random letter from the alphabet
+                    x2_letters.append(np.random.choice(range(n_letters)))
+                x2_drawers.append(np.random.choice(range(num_drawings)))
+                x2_images.append(
+                    alphabet_list[x2_alphabet_index][x2_letters[-1]][x2_drawers[-1]]
+                )
+
+            # Create the labels by comparing indexes
+            _labels = []
+            # Run through all the images and return same or different
+            for alph2, letter1, letter2 in zip(x2_alphabets, x1_letters, x2_letters):
+                # We know it's the same letter only if the alphabet indexes are the same
+                # and the letter indexes are also the same
+                if alph2 == i and letter1 == letter2:
+                    _labels.append(1)
+                else:
+                    _labels.append(0)
+
+            # Now we have the indexes of alphabets, letters and drawers for X2, so we just retrieve the images
+            # We have the X1's in x1_images
+            # We have the X2's in x2_images
+            # We have the labels
+            # Turn the arrays into (N,1,105,105)
+            x1_images = np.expand_dims(x1_images, 1)
+            x2_images = np.expand_dims(x2_images, 1)
+            # Concatenate the arrays into (N,2,105,105)
+            pairs = np.concatenate((x1_images, x2_images), axis=1)
+
+            if final_res is None:
+                final_res = pairs
+                alph_lab = _labels
+            else:
+                # Concatenate to add to the first axis
+                final_res = np.concatenate((final_res, pairs), axis=0)
+                alph_lab = np.hstack((alph_lab, _labels))
+
+        if dataset is None:
+            dataset = final_res
+            labels = alph_lab
+        else:
+            dataset = np.concatenate((dataset, final_res), axis=0)
+            labels = np.hstack((labels, alph_lab))
+        print(dataset.shape)
+        print(f"Alphabet {i}")
+
+    # Save everything
+    with open(f"data/processed/trainX_{int(size)}k.npy", "wb") as f:
+        np.save(f, dataset)
+    with open(f"data/processed/trainY_{int(size)}k.npy", "wb") as f:
+        np.save(f, labels)
